569fa0af2262a5079ff506e6852dd656f94cd501
Whit Morgan <webdevwhit@gmail.com>
2022-06-06 19:43:09 -0400

    added main.js

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -6,7 +6,7 @@ async function makeReq(){
         const res = await fetch(`https://cartridge-api-test.herokuapp.com/api/${cartridgeName}`)
 
         const data = res.json()
-
+        document.querySelector('#showCartridgeName').innerText = data.name
         console.log(data)
 }catch(error){
     console.error(error)
